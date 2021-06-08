4d97547ee9c14fd9f0f6552d826ee268ab8e0aef
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-06-08 20:06:33 -0300

    using gatsbylink

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,15 @@
-import React from "react"
+import React from 'react'
+import { Link } from 'gatsby'
 
 const AboutPage = () => {
     return (
         <div>
             <h1>about</h1>
+            <ul>
+                <Link to="/" activeStyle={{ border: 'solid red 1px' }}>
+                    Home
+                </Link>
+            </ul>
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,23 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import * as React from 'react'
+import { Link } from 'gatsby'
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from '../components/layout'
+import Seo from '../components/seo'
 
 const IndexPage = () => (
     <Layout>
         <Seo title="Home" />
-        <h1>Hi blog aas</h1>
+        <h1>Hi blog aaassas</h1>
+        <ul>
+            <li>
+                <Link to="/about">About</Link>
+            </li>
+            <li>
+                <Link to="/" activeStyle={{ border: 'solid red 1px' }}>
+                    Home
+                </Link>
+            </li>
+        </ul>
 
         <p>
             <Link to="/page-2/">Go to page 2</Link> <br />
