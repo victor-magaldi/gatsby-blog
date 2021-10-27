d4d368ab8bf9d4269855f59eff2ae03300183ddc
victor de souza magaldi <victmagaldi@hotmail.com>
2021-10-27 19:36:40 -0300

    Create e editing variables css

--- src/components/Layout/styled.js
+++ src/components/Layout/styled.js
@@ -4,7 +4,7 @@ export const LayoutWrapper = styled.section`
     display: flex;
 `
 export const LayoutMain = styled.main`
-    background-color: #16202c;
+    background-color: var(--background);
     min-height: 100vh;
     padding: 0 3.75rem 0 20rem;
     width: 100%;
--- src/components/Sidebar/styled.js
+++ src/components/Sidebar/styled.js
@@ -2,8 +2,8 @@ import styled from 'styled-components'
 
 export const SidebarWrapper = styled.aside`
     align-items: center;
-    border-right: 1px solid #38444d;
-    background: #fff;
+    border-right: 1px solid var(--borders);
+    background: var(--mediumBackground);
     display: flex;
     flex-direction: column;
     height: 100vh;
