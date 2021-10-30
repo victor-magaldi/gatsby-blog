d05512caa883fa34767155423c99cc5ac8c2e434
victor de souza magaldi <victmagaldi@hotmail.com>
2021-10-29 23:11:47 -0300

    instalation e effect anylink

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,8 @@ module.exports = {
         `gatsby-plugin-styled-components`,
         `gatsby-plugin-react-helmet`,
         `gatsby-plugin-image`,
+        'gatsby-plugin-transition-link',
+
         {
             resolve: `gatsby-source-filesystem`,
             options: {
