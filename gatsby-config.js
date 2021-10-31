040c99422d4961277b1522d9dd96948ae286d5ac
victor de souza magaldi <victmagaldi@hotmail.com>
2021-10-31 15:23:23 -0300

    config plugin manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,11 +76,11 @@ module.exports = {
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
+                name: `Victor Dev Blog`,
+                short_name: `blog dev`,
                 start_url: `/`,
-                background_color: `#663399`,
-                theme_color: `#663399`,
+                background_color: `#283040`,
+                theme_color: `#283040`,
                 display: `minimal-ui`,
                 icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
             },
